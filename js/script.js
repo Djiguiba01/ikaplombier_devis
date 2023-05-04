ac0d9c2b46385c4d7d06e185dfb3070beb5bc028
Djiguiba <djiguibabarema@gmail.com>
2023-05-04 07:07:09 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Menu:::::::
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+// Activé le menu en cas de click
+// menu.onclik = () =>{
+//     menu.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// }
+
+// window.onscroll = () =>{
+//     menu.classList.remove('fa-times');
+//     navbar.classList.remove('active');
+// }
+// :::::::::::::
